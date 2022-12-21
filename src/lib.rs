047401adf778124f3ea9b9ca0d843675978e6283
Akihito KIRISAKI <kirisaki@klara.works>
2022-12-21 09:54:14 +0900

    feat: define parse

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+pub enum JsonValue {
+    Number (f64),
+    Str (String),
+    Bool (bool),
+    Null,
+    List (Vec<Box<JsonValue>>),
+    Object (HashMap<String, Box<JsonValue>>),
+}
+
+pub fn parse(src: &str) -> (Result<JsonValue, &str>, &str) {
+    let (head0, tail) = src.split_at(1);
+    let head = head0.chars().nth(1).unwrap();
+    match head {
+        '{' => unimplemented!(),
+        '[' => unimplemented!(),
+        '"' => unimplemented!(),
+        ' ' | '\t' | '\n' | '\r' => unimplemented!(),
+        c if ('0' .. '9').contains(&c) => unimplemented!(),
+        _ => (Err("invalid character"), tail),
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
